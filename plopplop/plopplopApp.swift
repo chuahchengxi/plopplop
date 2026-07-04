c2f9cb2e7e6f0ec7971d8f5438a868c30345ad6f
chuahchengxi <boyboychuah@hotmail.com>
2026-07-04 09:56:49 +0800

    Initial Commit

---
+++ plopplop/plopplopApp.swift
@@ -0,0 +1,17 @@
+//
+//  plopplopApp.swift
+//  plopplop
+//
+//  Created by cheng xi on 4/7/26.
+//
+
+import SwiftUI
+
+@main
+struct plopplopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
